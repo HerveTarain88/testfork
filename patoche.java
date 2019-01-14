3c4e0943ad3d66816f2e8e240b727455d1cd59cc
Hervé <jfacchino@humanbooster.com>
2019-01-14 12:08:15 +0100

    second commit

--- patoche.java
+++ patoche.java
@@ -1,3 +1,5 @@
 salut c'est patoche, je suis moche mais je galoche
 
 Salut Patoche moi c Arlette je suis une mauviette mais j'adore la galette !
+
+Arlette j'aurais préféré que tu sois pas une mauviette mais une super bête qui aime la levrette
